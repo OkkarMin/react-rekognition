1c3812bb2ec8b65409b8f4ef521207bd7d716fcb
JIAYIN <jiayinjyjy@gmail.com>
2019-10-16 21:17:57 +0800

    Added columns

--- src/pages/view-attendance.js
+++ src/pages/view-attendance.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Loader } from 'semantic-ui-react'
+import { Loader, Button } from 'semantic-ui-react'
 
 import MUIDataTable from 'mui-datatables'
 
@@ -7,27 +7,11 @@ import Layout from '../components/layout'
 
 const url = 'http://ec2-3-15-165-103.us-east-2.compute.amazonaws.com/api'
 
-// const courseOptions = [
-//   { key: 'TSA1', text: 'TSA1', value: 'TSA1' },
-//   { key: 'f', text: 'Female', value: 'female' },
-// ]
-// const groupOptions = [
-//   { key: 'a', text: 'A', value: 'a' },
-//   { key: 'b', text: 'B', value: 'b' },
-// ]
-//const columns = [ "Status", "Matriculation No.","GroupID","Course Code"];
+//const columns = [ "Course code", "Group id",,"classtype" ,"matriculation No.","status"];
 const columns = [
   {
-    name: 'matriculation No.',
-    label: 'Matriculation No.',
-    options: {
-      filter: false,
-      sort: false,
-    },
-  },
-  {
-    name: 'status',
-    label: 'Status',
+    name: 'course code',
+    label: 'Course Code',
     options: {
       filter: true,
       sort: true,
@@ -42,8 +26,24 @@ const columns = [
     },
   },
   {
-    name: 'course code',
-    label: 'Course Code',
+    name: 'class type',
+    label: 'Class Type',
+    options: {
+      filter: true,
+      sort: true,
+    },
+  },
+  {
+    name: 'matriculation No.',
+    label: 'Matriculation No.',
+    options: {
+      filter: true,
+      sort: true,
+    },
+  },
+  {
+    name: 'status',
+    label: 'Status',
     options: {
       filter: true,
       sort: true,
@@ -94,32 +94,95 @@ const fetchAttendanceData = async (courseID, groupID) => {
 const getAttendanceData = async setData => {
   let data = []
 
-  let result1 = await fetchAttendanceData('CZ3000', 'SSA1')
-  let result2 = await fetchAttendanceData('CZ3002', 'TSA1')
+  //courseID , groupID
+  let result3000SSA1 = await fetchAttendanceData('CZ3000', 'SSA1')
+  let result3000LEC1 = await fetchAttendanceData('CZ3000', 'LEC1')
 
-  console.log(result1)
+  let result2000LEC1 = await fetchAttendanceData('CZ2000', 'LEC1')
+  let result2000LEC2 = await fetchAttendanceData('CZ2000', 'LEC2')
+  let result2000TSA1 = await fetchAttendanceData('CZ2000', 'TSA1')
+  let result2000TSA2 = await fetchAttendanceData('CZ2000', 'TSA2')
+  let result2000TSA3 = await fetchAttendanceData('CZ2000', 'TSA3')
 
-  result1.map(each => {
+  console.log(result3000SSA1)
+
+  //push in this order [ "Course code", "Group id","class type" ,"matriculation No.","status"];
+  result3000SSA1.map(each => {
     let eachStudentData = []
-    eachStudentData.push(each.matricNo)
-    eachStudentData.push(each.status)
+    eachStudentData.push('CZ3000')
     eachStudentData.push(each.groupID)
     eachStudentData.push(each.classType)
-    eachStudentData.push('CZ3000')
-
-    data.push(eachStudentData)
-  })
-
-  result2.map(each => {
-    let eachStudentData = []
     eachStudentData.push(each.matricNo)
     eachStudentData.push(each.status)
-    eachStudentData.push(each.groupID)
-    eachStudentData.push('CZ3002')
 
     data.push(eachStudentData)
   })
 
+  result3000LEC1.map(each => {
+    let eachStudentData = []
+    eachStudentData.push('CZ3000')
+    eachStudentData.push(each.groupID)
+    eachStudentData.push(each.classType)
+    eachStudentData.push(each.matricNo)
+    eachStudentData.push(each.status)
+
+    data.push(eachStudentData)
+  })
+
+  result2000LEC1.map(each => {
+    let eachStudentData = []
+    eachStudentData.push('CZ2000')
+    eachStudentData.push(each.groupID)
+    eachStudentData.push(each.classType)
+    eachStudentData.push(each.matricNo)
+    eachStudentData.push(each.status)
+
+    data.push(eachStudentData)
+  })
+
+  result2000LEC2.map(each => {
+    let eachStudentData = []
+    eachStudentData.push('CZ2000')
+    eachStudentData.push(each.groupID)
+    eachStudentData.push(each.classType)
+    eachStudentData.push(each.matricNo)
+    eachStudentData.push(each.status)
+
+    data.push(eachStudentData)
+  })
+
+  result2000TSA1.map(each => {
+    let eachStudentData = []
+    eachStudentData.push('CZ2000')
+    eachStudentData.push(each.groupID)
+    eachStudentData.push(each.classType)
+    eachStudentData.push(each.matricNo)
+    eachStudentData.push(each.status)
+
+    data.push(eachStudentData)
+  })
+
+  result2000TSA2.map(each => {
+    let eachStudentData = []
+    eachStudentData.push('CZ2000')
+    eachStudentData.push(each.groupID)
+    eachStudentData.push(each.classType)
+    eachStudentData.push(each.matricNo)
+    eachStudentData.push(each.status)
+
+    data.push(eachStudentData)
+  })
+
+  result2000TSA3.map(each => {
+    let eachStudentData = []
+    eachStudentData.push('CZ2000')
+    eachStudentData.push(each.groupID)
+    eachStudentData.push(each.classType)
+    eachStudentData.push(each.matricNo)
+    eachStudentData.push(each.status)
+
+    data.push(eachStudentData)
+  })
   setData(data)
 }
 
