7e4dad5a6d5344d449a3c7060719f57def653cf5
Me <jiayinjyjy@gmail.com>
2019-10-22 09:54:08 +0800

    Edited format of message

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,20 @@ const IndexPage = () => (
       Hello!
     </h2>
 
-    <div>
     Everything is pre set-up and ready to go! All you need to do is to upload the course student list containing student matriculation ids and the student’s pictures.
 
+     <br/>
+    <br />
      
              Once done, enjoy our efficient system of keeping track of student’s attendance. 
-               
+               <br />
              Thank you for using our services and have a fabulous day!
+             <br />
+    <br />
              Best Regards,
+             <br />
              ASEFaceRekog
-    </div>
-
+ 
   </Layout>
 )
 
